7bcaeea8daadea876b5a2377f0e061c24151fa9f
mohamedfareed12 <167489107+mohamedfareed12@users.noreply.github.com>
2024-05-04 22:02:02 +0300

    Create ExploreMenu.jsx

---
+++ frontend/src/Components/ExploreMenu/ExploreMenu.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import "./ExploreMenu.css";
+import { menu_list } from "../../assets/assets";
+
+const ExploreMenu = ({ category, setCategory }) => {
+  return (
+    <div className="explore-menu" id="explore-menu">
+      <h1>Explore our menu</h1>
+      <p className="explore-menu-text">
+        Discover a wide-ranging menu, offering an enticing variety of culinary
+        creations. Our goal is to delight your taste buds and enrich your dining
+        adventure, one memorable dish at a time."
+      </p>
+      <div className="explore-menu-list">
+        {menu_list.map((item, index) => {
+          return (
+            <div
+              onClick={() =>
+                setCategory((prev) =>
+                  prev === item.menu_name ? "All" : item.menu_name
+                )
+              }
+              key={index}
+              className="explore-menu-list-item"
+            >
+              <img
+                className={category === item.menu_name ? "active" : ""}
+                src={item.menu_image}
+                alt=""
+              />
+              <p>{item.menu_name}</p>
+            </div>
+          );
+        })}
+      </div>
+      <hr />
+    </div>
+  );
+};
+
+export default ExploreMenu;
